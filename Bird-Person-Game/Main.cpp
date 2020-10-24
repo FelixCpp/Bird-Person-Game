251a862c4e3957b79a24808ebb4feb70b571b087
FelixCPP <fix.busch@gmail.com>
2020-10-24 18:20:07 +0200

    SFML eingebunden / auf 64x gestellt und standard SFML-Code hinzugefügt

--- Bird-Person-Game/Main.cpp
+++ Bird-Person-Game/Main.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 int main()
 {
-	std::cout << "Hello, world" << std::endl;
-	std::cin.get();
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Welcome to SFML");
+	window.setFramerateLimit(60);
+
+	sf::CircleShape circle(100.f);
+	circle.setFillColor(sf::Color::Green);
+
+	sf::Event event;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
+
 	return EXIT_SUCCESS;
 }
